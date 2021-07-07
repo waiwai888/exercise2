cf47334b028d91343986adca8fce1fab0e5b8ee6
waiwai888 <yuisato9688@gmail.com>
2021-07-07 06:30:41 +0000

    [finish]応用４復習

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings'
+    get 'followers' => 'relationships#followers'
+  end
   resources :books do
     resources :book_comments,only: [:create,:destroy]
     resource :favorites,only: [:create,:destroy]
